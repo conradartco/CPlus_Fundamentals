055670938361e35993978da16767187f0c26484f
Jonathan Conrad <jc@Jonathans-MBP.home>
2022-09-08 16:38:28 -0700

    Changed source control folder to include all projects within this series

---
+++ Project001/main.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+/*
+* This is the main.cpp file
+* The application prints various lines to the console
+* By JC
+*/
+int main()
+{
+    // Start of print statements
+    printf("Hello World!\n");
+    printf("I am from Scotia, NY.\n");
+    printf("I enjoy gaming!\n");
+}
