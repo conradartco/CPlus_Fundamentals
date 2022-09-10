5f28042b66eb9d892bfbca314cef98a65ec58e2b
Jonathan Conrad <jc@Jonathans-MBP.home>
2022-09-10 11:49:55 -0700

    feat: Updated app to include delta time for more accurate frame rate

--- Project003/main.cpp
+++ Project003/main.cpp
@@ -9,49 +9,65 @@ int main()
     InitWindow(winWidth, winHeight, "Dapper Dasher");
 
     // acceleration due to gravity (pixels/frame)/frame
-    const int gravity{1};
+    const int gravity{1'000};
 
-    // rectangle dimensions
-    const int width{50};
-    const int height{80};
+    Texture2D scarfy = LoadTexture("textures/scarfy.png");
+    Rectangle scarfyRec;
+    scarfyRec.width = scarfy.width/6;
+    scarfyRec.height = scarfy.height;
+    scarfyRec.x = 0;
+    scarfyRec.y = 0;
+    Vector2 scarfyPos;
+    scarfyPos.x = winWidth/2 - scarfyRec.width/2;
+    scarfyPos.y = winHeight -scarfyRec.height;
 
-    int posY{winHeight - height};
     int velocity{0};
+
+    // is the rectangle in the air?
+    bool isInAir{false};
+    // jump velocity (pixels/sec)
+    const int jumpVel{-600};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+        // delta time (time since last frame)
+        const float dT{GetFrameTime()};
+
         // Start drawing
         BeginDrawing();
         ClearBackground(WHITE);
         // Game logic starts here:
 
         // perform ground check
-        if (posY >= winHeight - height)
+        if (scarfyPos.y >= winHeight - scarfyRec.height)
         {
-            // rectangle is on the ground
+            // scarfy is on the ground
             velocity = 0;
+            isInAir = false;
         }
         else
         {
-            // rectangle is in the air
-            velocity += gravity;
+            // scarfy is in the air
+            velocity += gravity * dT;
+            isInAir = true;
         }
 
-        // move rectangle
-        if (IsKeyDown(KEY_SPACE))
+        // jump check
+        if (IsKeyDown(KEY_SPACE) && !isInAir)
         {
-            velocity -= 10;
+            velocity += jumpVel;
         }
 
         // update position
-        posY += velocity;
+        scarfyPos.y += velocity * dT;
 
-        DrawRectangle(winWidth/2, posY, width, height, BLUE);
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
         
 
         // Game logic ends here:
         EndDrawing();
     }
+    UnloadTexture(scarfy);
     CloseWindow();
 }
