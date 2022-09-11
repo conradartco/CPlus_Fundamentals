f27e09ef43a65030ee95b81e12653592b19fd48b
Jonathan Conrad <jc@Jonathans-MBP.home>
2022-09-10 22:26:22 -0700

    feat: Added Prop class and added functionality to collide with props placed in the world

--- ClassyClash/Character.cpp
+++ ClassyClash/Character.cpp
@@ -1,21 +1,20 @@
 #include "Character.h"
 #include "raymath.h"
 
-Character::Character()
+Character::Character(int winWidth, int winHeight)
 {
-    width = texture.width/maxFrames;
+    width = texture.width / maxFrames;
     height = texture.height;
-}
 
-void Character::setScreenPos(int winWidth, int winHeight)
-{
     screenPos = {
-        (float)winWidth / 2.0f - 4.0f * (0.5f * width),
-        (float)winHeight / 2.0f - 4.0f * (0.6f * height)};
+        static_cast<float>(winWidth) / 2.0f - scale * (0.5f * width),
+        static_cast<float>(winHeight) / 2.0f - scale * (0.6f * height)};
 }
 
 void Character::tick(float deltaTime)
 {
+    worldPosLastFrame = worldPos;
+
     Vector2 direction{};
     if (IsKeyDown(KEY_A))
         direction.x -= 1.0;
@@ -50,8 +49,22 @@ void Character::tick(float deltaTime)
 
     // draw the character
     Rectangle source{frame * width, 0.f, rightLeft * width, height};
-    Rectangle dest{screenPos.x, screenPos.y, 4.0f * width, 4.0f * height};
+    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
     Vector2 origin{};
     DrawTexturePro(texture, source, dest, origin, 0.f, WHITE);
 }
 
+void Character::undoMovement()
+{
+    worldPos = worldPosLastFrame;
+}
+
+Rectangle Character::getCollisionRec()
+{
+    return Rectangle{
+        screenPos.x,
+        screenPos.y,
+        width * scale,
+        height * scale
+    };
+}
--- ClassyClash/main.cpp
+++ ClassyClash/main.cpp
@@ -1,22 +1,27 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "Character.h"
+#include "Prop.h"
 
 int main()
 {
-    int winDims[2];
-    winDims[0] = 384;
-    winDims[1] = 384;
+    const int windowWidth{384};
+    const int windowHeight{384};
 
     // initialize window
-    InitWindow(winDims[0], winDims[1], "Classy Clash");
+    InitWindow(windowWidth, windowHeight, "Classy Clash");
 
     // load background
     Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
     Vector2 mapPos{0.0, 0.0};
+    const float mapScale{4.0f};
 
-    Character knight;
-    knight.setScreenPos(winDims[0], winDims[1]);
+    Character knight{windowWidth, windowHeight};
+
+    Prop props[2]{
+        Prop{Vector2{800.f, 500.f}, LoadTexture("nature_tileset/Rock.png")},
+        Prop{Vector2{600.f, 700.f}, LoadTexture("nature_tileset/Log.png")}
+    };
 
     SetTargetFPS(60);
 
@@ -29,10 +34,33 @@ int main()
         mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
         // draw the map
-        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
-        knight.tick(GetFrameTime());
+        DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
 
-        // game logic
+        // draw the props
+        for (auto prop : props)
+        {
+            prop.Render(knight.getWorldPos());
+        }
+
+        knight.tick(GetFrameTime());
+        // check map bounds
+        if (knight.getWorldPos().x < 0.f || 
+        knight.getWorldPos().y < 0.f || 
+        knight.getWorldPos().x + windowWidth > map.width * mapScale || 
+        knight.getWorldPos().y + windowHeight > map.height * mapScale)
+        {
+            knight.undoMovement();
+        }
+
+        // check prop collisions
+        for (auto prop : props)
+        {
+            if (CheckCollisionRecs(prop.getCollisionRec(knight.getWorldPos()), knight.getCollisionRec()))
+            {
+                knight.undoMovement();
+            }
+        }
+        
 
 
         EndDrawing();
